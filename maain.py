c7ded1b84bd08c7f4cf9105052cedc0186a40fec
Armandito18 <lopezpattatuchiarmandoernesto@gmail.com>
2026-04-27 17:59:16 -0600

    Se integra main.py con llamadas a funciones

---
+++ maain.py
@@ -0,0 +1,7 @@
+from funcion_n import suma, resta, multiplicacion
+r1 = suma(4,4)
+r2 = resta(10,5)
+r3 = multiplicacion(20,2)
+print("La suma resultante es:", r1)
+print("La resta resultante es:", r2)
+print("La multiplicacion resultante es:", r3)
